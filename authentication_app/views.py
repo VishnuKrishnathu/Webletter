d9ed64574cef7e0360d1634ba358595e745fce77
Vishnu Krishnathu <vishnu.krishnathu1999@gmail.com>
2022-01-05 22:36:19 +0530

    Register user view added

--- authentication_app/views.py
+++ authentication_app/views.py
@@ -1,25 +1,44 @@
 from django.shortcuts import render
 
 # Create your views here.
+import io
+from rest_framework.parsers import JSONParser
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .models import CustomUser
+from .serializers import UserSerializer
+import jwt
+import environ   # importing django-environ to read env files
+
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
+
+### converts JSON object to python data type
+def JSONToDict(json_object):
+    stream = io.BytesIO(json_object)
+    data = JSONParser().parse(stream)
+    return data
+
 
 @api_view(['POST'])
 def registerUser(request):
+    user = UserSerializer(data = JSONToDict(request.body))
+    if(user.is_valid()):
+        user.save()
+        encoded_jwt = jwt.encode({
+            "username": user.data['username'],
+            "password": user.data['password'],
+            "email": user.data['password']
+        }, env('ACCESS_TOKEN'))
+        return Response({'token' : encoded_jwt}, 200)
 
-    username = request.data['username']
-    first_name = request.data['first_name']
-    last_name = request.data['last_name']
-    email = request.data['email']
-    password = request.data['password']
-    user = CustomUser(username = username, first_name = first_name, last_name= last_name, email = email, password = password)
-    print(user)
-
-    user.save()
+    else:
+        return Response({'message' : 'error saving user informaton. Try again'}, 400)
 
 
-    return Response({
-        "message" : "Hello World"
-    }, 200)
+@api_view(['POST'])
+def loginUser(request):
+    pass
