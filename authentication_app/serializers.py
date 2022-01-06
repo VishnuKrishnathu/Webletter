b6eca123acc132e7a7d58bc14419d56784e2f9c0
Vishnu Krishnathu <vishnu.krishnathu1999@gmail.com>
2022-01-06 11:54:43 +0530

    Login logout functionality added
    Changes made
        - Added serializers.py to serialize User model
        - login-user url added
        - loginUser view added to views.py
        - jwt token in registeration is sent over httpOnly cookie

--- authentication_app/serializers.py
+++ authentication_app/serializers.py
@@ -7,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password']
-        extra_kwargs = { 'password' : { 'write_only' : True } }
+        # extra_kwargs = { 'password' : { 'write_only' : True } }
 
     def create(self, validated_data):
         user = User(
--- authentication_app/views.py
+++ authentication_app/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
-
 # Create your views here.
 import io
 from rest_framework.parsers import JSONParser
-
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.contrib.auth import authenticate
 from .serializers import UserSerializer
 import jwt
+import json
 import environ   # importing django-environ to read env files
 
 
@@ -22,6 +21,10 @@ def JSONToDict(json_object):
     data = JSONParser().parse(stream)
     return data
 
+def DictToBString(dict_object):
+    return str(json.dumps(dict_object).encode('utf-8'))
+
+
 
 @api_view(['POST'])
 def registerUser(request):
@@ -33,7 +36,10 @@ def registerUser(request):
             "password": user.data['password'],
             "email": user.data['password']
         }, env('ACCESS_TOKEN'))
-        return Response({'token' : encoded_jwt}, 200)
+
+        response = Response({'message': 'User has been successfully created'})
+        response.set_cookie(key="token", value=encoded_jwt, max_age=60*60*24, httponly=True)
+        return response
 
     else:
         return Response({'message' : 'error saving user informaton. Try again'}, 400)
@@ -41,4 +47,15 @@ def registerUser(request):
 
 @api_view(['POST'])
 def loginUser(request):
-    pass
+    data = JSONToDict(request.body)
+    user = authenticate(
+        username = data['username'],
+        password = data['password']
+    )
+
+    print(user)
+    if user == None:
+        return Response({'message' : 'Check the login credentials and try again'}, 400)
+
+    else:
+        return Response({'message' : 'User is now successfully logged in'}, 200)
